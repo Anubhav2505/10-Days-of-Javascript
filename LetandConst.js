ba56df7dcf7f0321709dda7bd4258b60b6a5ed57
Anubhav Deshwal <46739402+Anubhav2505@users.noreply.github.com>
2020-10-27 21:20:34 +0530

    Day 1: Let and Const

---
+++ LetandConst.js
@@ -0,0 +1,12 @@
+
+function main() {
+   let r = parseFloat(readLine());
+    const PI=Math.PI;
+     // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    
+    // Print the area of the circle:
+
+    console.log(PI*r*r);
+    
+    // Print the perimeter of the circle:
+    console.log(2*PI*r);
